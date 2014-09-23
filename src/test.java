c3e0e0ea0b38e194bc7efe1acb7eddc97c5b5960
end313124 <end313124@gmail.com>
2014-09-23 18:23:40 +0900

    get data from csv file and return as List of List.
    this method didn't work before.

--- src/test.java
+++ src/test.java
@@ -1,40 +1,124 @@
 import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
 import java.io.IOException;
 import java.util.Random;
 
 import javax.naming.spi.DirStateFactory.Result;
-
+import javax.swing.text.PlainDocument;
 public class test {
-
+	private String filename;
 	/**
 	 * @param args
 	 */
-	public static String testMeth() {
-		String rt;
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	// get file size
+	public static int getFileSize() {
+		int sz = 0;
 		try {
 			// read file
 			FileReader data = new FileReader("/home/k/workspace/0favTweetBot/test.csv");
-			System.out.println("0");
 			BufferedReader br = new BufferedReader(data);
 			// deal with data as line
 			String line;
 			StringTokenizer token;
 			Random rnd = new Random();
-			
-			
-			
 			// size of rows
-			int sz = 0;
 			while ((line = br.readLine()) != null) {
 				// separation sign is comma
 				sz = sz + 1;
-				System.out.println(sz);
 			}
-									
+			data.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return 0 ;
+		}		
+		return sz;
+	}
+	
+
+	public List<List<String>> readFile() {
+		
+		// input check
+		if(this.filename == null){
+			return null;		
+		}
+		List<List<String>> retBox = new ArrayList<List<String>>();
+		
+		File csv = new File(filename);
+		
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(csv));
+            String line = "";
+            int idx = 0;
+            while ((line = br.readLine()) != null) {
+ 
+                // 1行をデータの要素に分割
+                StringTokenizer st = new StringTokenizer(line, ",");
+ 
+                // 1行を格納する箱作成
+                List<String> tmpList = new ArrayList<String>();
+ 
+                while (st.hasMoreTokens()) {
+ 
+                    String str = st.nextToken().trim();
+                    // 1行の各要素を格納
+                    tmpList.add(str);
+ 
+                }
+ 
+                // 返却用リストに1行データを格納
+                retBox.add(tmpList);
+                idx++;
+ 
+                // numを超えたら読み込み終了。numが0のときは全量読む。
+                if( getFileSize() != 0 && idx > getFileSize() ){
+                    break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+ 
+        return retBox;
+    }
+
+
+
+			
+			
+			/*
 			int i = rnd.nextInt(sz)	+ 1;
 			System.out.println(i);
+		
+			
+		}
+		
+		
+		
+		String rt;
+		
+		
 			
 			//ここまでOK
 			//うまくいかないっぽいので http://kei0310.info/?p=240 をみて，書きなおすこと．
@@ -44,7 +128,7 @@ public class test {
 				// separation sign is comma
 				System.out.print("in!");
 				token = new StringTokenizer(line,",");
-				if (token.nextToken().equals("3")) {/*String.valueOf(sz))) {*/
+				if (token.nextToken().equals("3")) { 
 					rt = (token.nextToken() + token.nextToken());
 					System.out.println(rt);
 				}
@@ -56,11 +140,17 @@ public class test {
 		}
 		// TODO 自動生成されたメソッド・スタブ
 		return "dead";
-	}
+	}  */
 
-		public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.print(testMeth());
-	}
-	
+		public static List<List<String>> getListList() {
+		//TODO 自動生成されたメソッド・スタブ
+		System.out.println(getFileSize());
+		//CSVファイル読み込み
+		test reader = new test();
+		reader.setFilename("/home/k/workspace/0favTweetBot/test.csv");
+		//CSVデータ取得
+		List<List<String>> csv = reader.readFile();
+		return csv;
+		}
+
 }
